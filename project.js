6104a6f05fe6eb0469264239126ff5320734a464
_charan_05_ <23501a0595@pvpsit.ac.in>
2025-03-31 12:23:01 +0530

    2 player changed

--- project.js
+++ project.js
@@ -6,14 +6,34 @@ let arr=[
 
 let mode;
 let count=1;
+let result;
+
 let modeButton=document.querySelectorAll(".js-mode");
 modeButton.forEach((button)=>{
     button.addEventListener("click",()=>{
-        if(button.innerHTML==="One Player")
+        if(button.innerHTML==="One Player"){
             mode=1;
-        else
+            result=true;
+            count=1;
+            arr=[
+                [0,0,0],
+                [0,0,0],
+                [0,0,0]
+            ];
+        }
+        else{
             mode=2;
-        document.querySelector(".js-parent").innerHTML=`<div class="superChild">
+            result=true;
+            count=1;
+            arr=[
+                [0,0,0],
+                [0,0,0],
+                [0,0,0]
+            ];
+        }
+        document.querySelector(".js-reset").innerHTML="TO RESET YOU CAN RESELECT YOUR GAME MODE"
+        document.querySelector(".js-parent").innerHTML=
+        `<div class="superChild">
             <button  onclick="move(0,0)" class="box" ><div class="child js-00"></div></button>
             <button  onclick="move(0,1)" class="box" ><div class="child js-01"></div></button>
             <button  onclick="move(0,2)" class="box" ><div class="child js-02"></div></button>
@@ -28,31 +48,48 @@ modeButton.forEach((button)=>{
             <button  onclick="move(2,1)" class="box" ><div class="child js-21"></div></button>
             <button  onclick="move(2,2)" class="box" ><div class="child js-22"></div></button>
         </div>
-        <p class="js-turn" align="center">X's move</p>`;
+        <p class="turn js-turn" align="center">X's move</p>`;
     })
 });
 
 function check(){
-    if(arr[0][0]===arr[1][1] && arr[1][1]===arr[2][2] && arr[0][0]!==0)
-        console.log(`${arr[0][0]} WON1`);
-    else if(arr[0][2]===arr[1][1] && arr[1][1]===arr[2][0] && arr[0][2]!==0)
-        console.log(`${arr[0][2]} WON2`);
-    else if(arr[0][0]===arr[1][0] && arr[1][0]===arr[2][0] && arr[0][0]!==0)
-        console.log(`${arr[0][0]} WON3`);
-    else if(arr[0][1]===arr[1][1] && arr[1][1]===arr[2][1] && arr[0][1]!==0)
-        console.log(`${arr[0][1]} WON4`);
-    else if(arr[0][2]===arr[1][2] && arr[1][2]===arr[2][2] && arr[0][2]!==0)
-        console.log(`${arr[0][2]} WON5`);
-    else if(arr[0][0]===arr[0][1] && arr[0][1]===arr[0][2] && arr[0][0]!==0)
-        console.log(`${arr[0][0]} WON6`);
-    else if(arr[1][0]===arr[1][1] && arr[1][1]===arr[1][2] && arr[1][0]!==0)
-        console.log(`${arr[1][0]} WON7`);
-    else if(arr[2][0]===arr[2][1] && arr[2][1]===arr[2][2] && arr[2][0]!==0)
-        console.log(`${arr[2][0]} WON8`);
+    if(arr[0][0]===arr[1][1] && arr[1][1]===arr[2][2] && arr[0][0]!==0){
+        document.querySelector(".js-turn").innerHTML=(`${arr[0][0]} WON1`);
+        return false;
+    }
+    else if(arr[0][2]===arr[1][1] && arr[1][1]===arr[2][0] && arr[0][2]!==0){
+        document.querySelector(".js-turn").innerHTML=(`${arr[0][2]} WON2`);
+        return false;
+    }
+    else if(arr[0][0]===arr[1][0] && arr[1][0]===arr[2][0] && arr[0][0]!==0){
+        document.querySelector(".js-turn").innerHTML=(`${arr[0][0]} WON3`);
+         return false;
+    }
+    else if(arr[0][1]===arr[1][1] && arr[1][1]===arr[2][1] && arr[0][1]!==0){
+        document.querySelector(".js-turn").innerHTML=(`${arr[0][1]} WON4`);
+        return false;
+    }
+    else if(arr[0][2]===arr[1][2] && arr[1][2]===arr[2][2] && arr[0][2]!==0){
+        document.querySelector(".js-turn").innerHTML=(`${arr[0][2]} WON5`);
+        return false;
+    }
+    else if(arr[0][0]===arr[0][1] && arr[0][1]===arr[0][2] && arr[0][0]!==0){
+        document.querySelector(".js-turn").innerHTML=(`${arr[0][0]} WON6`);
+        return false;
+    }
+    else if(arr[1][0]===arr[1][1] && arr[1][1]===arr[1][2] && arr[1][0]!==0){
+        document.querySelector(".js-turn").innerHTML=(`${arr[1][0]} WON7`);
+        return false;
+    }
+    else if(arr[2][0]===arr[2][1] && arr[2][1]===arr[2][2] && arr[2][0]!==0){
+        document.querySelector(".js-turn").innerHTML=(`${arr[2][0]} WON8`);
+        return false;
+    }
+    return true;
 }
 
 function move(a,b){
-    if(mode===2){
+    if(mode===2 && result){
         let block=document.querySelector(`.js-${a}${b}`);
         if((count%2)!==0){
             block.innerHTML="<p>X</p>";
@@ -65,7 +102,7 @@ function move(a,b){
             document.querySelector(".js-turn").innerHTML="X's move";
         }
         if(count>=5)
-            check();
+            result=check();
         count++;
     }
 }
